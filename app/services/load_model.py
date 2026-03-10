0156341a51a852b255b89e720672bc0ecc1216a0
jaiganesh-23 <jaiganeshmothilal22@gmail.com>
2026-03-10 16:55:04 +0530

    changed model

--- app/services/load_model.py
+++ app/services/load_model.py
@@ -5,5 +5,5 @@ model = None
 def get_model():
     global model
     if model is None:
-        model = SentenceTransformer("all-MiniLM-L6-v2")
+        model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
     return model
