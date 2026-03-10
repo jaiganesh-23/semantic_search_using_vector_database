ebd4422f2e7dd5c023ec36499134b1cee708b9fb
jaiganesh-23 <jaiganeshmothilal22@gmail.com>
2026-03-10 16:48:13 +0530

    added caching

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,9 +1,8 @@
-from sentence_transformers import SentenceTransformer
-
+from .load_model import get_model
 class EmbeddingService:
 
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = get_model()
 
     def embed(self, texts):
         return self.model.encode(texts)
